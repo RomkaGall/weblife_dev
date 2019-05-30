04ff9e3aa420e38a23f663f4a9b6b8581a6694e9
Roman Galaniuk <romangalanuk@gmail.com>
2019-05-31 00:00:36 +0300

    finish desktop

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,30 @@
 $(document).ready(function () {
   console.log("Скрипты подъехали")
 
-  $('.reviews__slider').slick({
+  $('.banner__slider').slick({
     slidesToShow: 1,
     dots: true,
-    arrows: false,
-    infinite: true
+    arrows: true,
+    infinite: true,
+    autoplay: true,
+    prevArrow: $('.banner').find('.arrow__prev'),
+    nextArrow: $('.banner').find('.arrow__next'),
   })
 
-  $('.menu_btn').click(function(){
+  $('.menu_btn').click(function () {
     $(this).toggleClass('open');
     $('.header__menu').toggleClass('show')
-	});
+  });
+
+  $('.header__link-bottom').hover(function() {
+    $('.overlay').toggleClass('active')
+  })
 
   //wow
   $(function () {
     new WOW({
-      offset:       200,
-      mobile:       false,       
+      offset: 200,
+      mobile: false,
     }).init();
   })
 });
